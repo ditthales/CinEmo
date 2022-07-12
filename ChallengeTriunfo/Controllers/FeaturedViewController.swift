402446c8ee6208dd01cf8f8b50108215cf0348bd
ditthales <thalesvgfraga@gmail.com>
2022-07-12 16:54:40 -0300

    Configuracao das imagens e collections

--- ChallengeTriunfo/Controllers/FeaturedViewController.swift
+++ ChallengeTriunfo/Controllers/FeaturedViewController.swift
@@ -7,26 +7,71 @@
 
 import UIKit
 
-class FeaturedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class FeaturedViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet var popularCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    @IBOutlet var upcomingCollectionView: UICollectionView!
+    
+    let popularMovies: [Movie] = Movie.popularMovies()
+    let nowPlayingMovies: [Movie] = Movie.nowPlayingMovies()
+    let upcomingMovies: [Movie] = Movie.upcomingMovies()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         popularCollectionView.dataSource = self
+        nowPlayingCollectionView.dataSource = self
+        upcomingCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        if collectionView == popularCollectionView{
+            return popularMovies.count
+        }else if collectionView == nowPlayingCollectionView{
+            return nowPlayingMovies.count
+        }else if collectionView == upcomingCollectionView{
+            return upcomingMovies.count
+        }else{
+            return 1
+        }
+        
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell
-        cell?.image.image = UIImage(named: "popular")
-        cell?.titleLabel.text = "Título do Filme"
-
-        return cell ?? UICollectionViewCell()
+        
+        if collectionView == popularCollectionView{
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell{
+                cell.image.image = UIImage(named: popularMovies[indexPath.row].backdrop)
+                cell.titleLabel.text = popularMovies[indexPath.row].title
+                return cell
+            }
+            
+            return UICollectionViewCell()
+        }else if collectionView == nowPlayingCollectionView{
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nowPlayingCell", for: indexPath) as? NowPlayingCollectionViewCell{
+                cell.image.image = UIImage(named: nowPlayingMovies[indexPath.row].poster)
+                cell.titleLabel.text = nowPlayingMovies[indexPath.row].title
+                cell.dateLabel.text = nowPlayingMovies[indexPath.row].releaseDate
+                return cell
+            }
+            
+            return UICollectionViewCell()
+        }else if collectionView == upcomingCollectionView{
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "upcomingCell", for: indexPath) as? UpcomingCollectionViewCell{
+                cell.image.image = UIImage(named: upcomingMovies[indexPath.row].poster)
+                cell.titleLabel.text = upcomingMovies[indexPath.row].title
+                cell.dateLabel.text = upcomingMovies[indexPath.row].releaseDate
+                return cell
+            }
+            
+            return UICollectionViewCell()
+        }else{
+            return UICollectionViewCell()
+        }
+        
         
     }
     
---
+++ ChallengeTriunfo/Model/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  ChallengeTriunfo
+//
+//  Created by ditthales on 12/07/22.
+//
+
+import Foundation
+
+struct Movie{
+    let title: String
+    let poster: String
+    let backdrop: String
+    let voteAverage: Float
+    let releaseDate: String
+    let overview: String
+    let genres: [String]
+    
+    
+}
